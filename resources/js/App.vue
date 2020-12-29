f03599838d358a99bf23f28ad11836cde2854c27
willywin99 <willywinsianturi@gmail.com>
2020-12-29 10:25:59 +0700

    Install Vuetify

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <div class="warpper">
-      <h2>SanberCode</h2>
-      <router-link to="/">Home</router-link>
-      <router-link to="/donations">Donations</router-link>
-      <router-link to="/blogs">Blogs</router-link>
-      <div class="content-wrapper">
-          <router-view></router-view>
-      </div>
-      <app-footer/>
-  </div>
+  <!-- App.vue -->
+
+    <v-app>
+        <v-navigation-drawer app>
+            Side Bar
+        </v-navigation-drawer>
+
+        <v-app-bar app>
+            Header
+        </v-app-bar>
+
+        <!-- Sizes your content based upon application components -->
+        <v-main>
+
+            <!-- Provides the application the proper gutter -->
+            <v-container fluid>
+
+            <!-- If using vue-router -->
+            <router-view></router-view>
+            </v-container>
+        </v-main>
+
+        <v-footer app>
+            @SanberCode 2020
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
