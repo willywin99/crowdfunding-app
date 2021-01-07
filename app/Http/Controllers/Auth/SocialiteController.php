c020dd7cf510ad128708811b8fc945a667b30b3d
willywin99 <willywinsianturi@gmail.com>
2021-01-08 05:39:56 +0700

    Login dengan Media Sosial 2

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 use Laravel\Socialite\Facades\Socialite;
@@ -17,5 +19,50 @@ class SocialiteController extends Controller
         return response()->json([
             'url' => $url
         ]);
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        try{
+            $social_user = Socialite::driver($provider)->stateless()->user();
+
+            // dd($social_user);
+
+            if(!$social_user){
+                return response()->json([
+                    'response_code' => '01',
+                    'response_message' => 'lagin failed',
+                ], 401);
+            }
+
+            $user = User::whereEmail($social_user->email)->first();
+
+            if(!$user) {
+                if($provider == 'google') {
+                    $photo_profile = $social_user->avatar;
+                }
+                $user = User::create([
+                    'email' => $social_user->email,
+                    'name' => $social_user->name,
+                    'email_verified_at' => Carbon::now(),
+                    'photo_profile' => $photo_profile
+                ]);
+            }
+
+            $data['user'] = $user;
+            $data['token'] = auth()->login($user);
+
+            return response()->json([
+                'response_code' => '00',
+                'response_message' => 'user berhasil login',
+                'data' => $data,
+            ], 200);
+
+        } catch(\Throwable $th) {
+            return response()->json([
+                'response_code' => '01',
+                'response_message' => 'login failed',
+            ], 401);
+        }
     }
 }
